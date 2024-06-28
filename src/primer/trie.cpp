929b67e7c1886eb83262853df0dee25347095bde
Vollate <uint44t@gmail.com>
2024-06-29 00:09:57 +0800

    C++ primer: task 1

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -1,8 +1,11 @@
 #include "primer/trie.h"
 #include <cstddef>
 #include <cstdint>
+#include <list>
 #include <memory>
 #include <string_view>
+#include <utility>
+#include <vector>
 #include "common/exception.h"
 
 namespace bustub {
@@ -14,10 +17,13 @@ auto Trie::Get(std::string_view key) const -> const T * {
   // dynamic_cast returns `nullptr`, it means the type of the value is mismatched, and you should return nullptr.
   // Otherwise, return the value.
 
+  if (root_ == nullptr) {
+    return nullptr;
+  }
   auto cur_root = root_;
   for (const auto key_char : key) {
     if (auto val = cur_root->children_.find(key_char); val != cur_root->children_.end()) {
-      cur_root = cur_root->children_.at(key_char);
+      cur_root = val->second;
     } else {
       return nullptr;
     }
@@ -37,55 +43,88 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
   // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
   // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
   // exists, you should creatSe a new `TrieNodeWithValue`.
+
   Trie ret{};
   if (key.empty()) {
-    ret.root_ = std::make_shared<TrieNode>(TrieNodeWithValue<T>(std::make_shared<T>(std::move(value))));
+    ret.root_ = std::make_shared<TrieNodeWithValue<T>>(root_->children_, std::make_shared<T>(std::move(value)));
     return ret;
   }
-  auto new_root = std::make_shared<TrieNode>();
-  ret.root_ = new_root;
+  std::shared_ptr<TrieNode> cur_new_root =
+      (root_ == nullptr) ? std::make_shared<TrieNode>() : std::shared_ptr<TrieNode>(root_->Clone());
+  auto last_new_root = cur_new_root;
+  ret.root_ = cur_new_root;
   auto old_root = root_;
-  std::shared_ptr<TrieNode> next_root = nullptr;
-  for (size_t i = 0; i < key.length() - 1; ++i) {
-    char key_char = key[i];
+  for (char step_key : key) {
     if (old_root != nullptr) {
-      new_root->children_ = old_root->children_;
-      if (old_root->children_.find(key_char) != old_root->children_.end()) {
-        old_root = old_root->children_.at(key_char);
+      cur_new_root->children_ = old_root->children_;
+      if (auto next_old_root = old_root->children_.find(step_key); next_old_root != old_root->children_.end()) {
+        cur_new_root->children_[step_key] = next_old_root->second->Clone();
+        old_root = next_old_root->second;
       } else {
+        cur_new_root->children_[step_key] = std::make_shared<TrieNode>();
         old_root = nullptr;
       }
+    } else {
+      cur_new_root->children_[step_key] = std::make_shared<TrieNode>();
     }
-    next_root = std::make_shared<TrieNode>();
-    new_root->children_[key_char] = next_root;
-    new_root = next_root;
+    last_new_root = cur_new_root;
+    cur_new_root = std::const_pointer_cast<TrieNode>(cur_new_root->children_[step_key]);
   }
-  new_root->children_.insert(
-      {key[key.length() - 1], std::make_shared<TrieNode>(TrieNodeWithValue<T>(std::make_shared<T>(std::move(value))))});
+  last_new_root->children_[key[key.length() - 1]] =
+      std::make_shared<TrieNodeWithValue<T>>(cur_new_root->children_, std::make_shared<T>(std::move(value)));
   return ret;
 }
 
 auto Trie::Remove(std::string_view key) const -> Trie {
   // You should walk through the trie and remove nodes if necessary. If the node doesn't contain a value any more,
   // you should convert it to `TrieNode`. If a node doesn't have children any more, you should remove it.
+  // FIXME: what happened if the key is not in the trie?
+
   Trie ret{};
   if (key.empty()) {
-    ret.root_ = std::make_shared<TrieNode>(root_->children_);
+    if (root_ != nullptr && !root_->children_.empty()) {
+      ret.root_ = std::make_shared<TrieNode>(root_->children_);
+    }
     return ret;
   }
-  auto cur_root = std::make_shared<TrieNode>();
-  ret.root_ = cur_root;
+  auto cur_new_root = std::make_shared<TrieNode>();
+  std::list<std::shared_ptr<TrieNode>> history;
+  ret.root_ = cur_new_root;
   auto old_root = root_;
-  for (size_t i = 0; i < key.length() - 1; ++i) {
-    cur_root->children_ = old_root->children_;
-    cur_root = cur_root->children_.at(key[i])->Clone();
+  for (char step_key : key) {
+    if (old_root != nullptr) {
+      cur_new_root->children_ = old_root->children_;
+      if (auto next_old_root = old_root->children_.find(step_key); next_old_root != old_root->children_.end()) {
+        cur_new_root->children_[step_key] = next_old_root->second->Clone();
+        old_root = next_old_root->second;
+      } else {
+        cur_new_root->children_[step_key] = std::make_shared<TrieNode>();
+        old_root = nullptr;
+      }
+    } else {
+      cur_new_root->children_[step_key] = std::make_shared<TrieNode>();
+    }
+    history.push_back(cur_new_root);
+    cur_new_root = std::const_pointer_cast<TrieNode>(cur_new_root->children_[step_key]);
   }
-  char last_char = key[key.length() - 1];
-  auto target = cur_root->children_.at(last_char);
-  if (target->children_.empty()) {
-    cur_root->children_.erase(last_char);
+  auto last_new_root = history.rbegin();
+  if (old_root != nullptr && old_root->children_.find(key[key.length() - 1]) != old_root->children_.end()) {
+    old_root = old_root->children_.find(key[key.length() - 1])->second;
+  }
+  if (old_root != nullptr && !old_root->children_.empty()) {
+    last_new_root->get()->children_[key[key.length() - 1]] = std::make_shared<TrieNode>(old_root->children_);
   } else {
-    target = std::make_shared<TrieNode>(target->children_);
+    last_new_root->get()->children_.erase(key[key.length() - 1]);
+  }
+  for (size_t i = 2; last_new_root != history.rend() && i <= key.length(); ++i) {
+    if (!last_new_root->get()->is_value_node_ && last_new_root->get()->children_.empty()) {
+      (++last_new_root)->get()->children_.erase(key[key.length() - i]);
+    } else {
+      break;
+    }
+  }
+  if (ret.root_->children_.empty()) {
+    ret.root_ = nullptr;
   }
   return ret;
 }
@@ -94,8 +133,8 @@ auto Trie::Remove(std::string_view key) const -> Trie {
 //
 // Generally people would write the implementation of template classes and functions in the header file. However, we
 // separate the implementation into a .cpp file to make things clearer. In order to make the compiler know the
-// implementation of the template functions, we need to explicitly instantiate them here, so that they can be picked up
-// by the linker.
+// implementation of the template functions, we need to explicitly instantiate them here, so that they can be picked
+// up by the linker.
 
 template auto Trie::Put(std::string_view key, uint32_t value) const -> Trie;
 template auto Trie::Get(std::string_view key) const -> const uint32_t *;
